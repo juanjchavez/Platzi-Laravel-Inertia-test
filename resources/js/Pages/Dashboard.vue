02eee9f76ed712b28deef7086180ae83f2431c81
Juan Chavez <juan.chavezn1990@gmail.com>
2021-05-03 11:09:50 -0600

    Configuración inicial para notas

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -5,12 +5,8 @@
                 Dashboard
             </h2>
         </template>
-
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
-                </div>
             </div>
         </div>
     </app-layout>
@@ -18,12 +14,10 @@
 
 <script>
     import AppLayout from '@/Layouts/AppLayout'
-    import Welcome from '@/Jetstream/Welcome'
 
     export default {
         components: {
             AppLayout,
-            Welcome,
         },
     }
 </script>
--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,18 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+}); 
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+/* Route::get('/', [App\Http\Controllers\PageController::class, 'index'])
+    ->name('index');
+ */
+Route::get('dashboard', [App\Http\Controllers\PageController::class, 'dashboard'])
+    ->middleware('auth:sanctum')
+    ->name('dashboard');
+
+Route::resource('notes',App\Http\Controllers\NoteController::class)
+    ->middleware('auth:sanctum');
+/* Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+ */
